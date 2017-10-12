e471be12efe9306eabe96de7cef3c39b3b451309
NarthVader <nathanberkenmeier@u.boisestate.edu>
2017-10-11 18:47:28 -0600

    fix

--- public/navigation.php
+++ public/navigation.php
@@ -4,6 +4,6 @@
         <li <?php if($thisPage == "League News") { echo 'id="currentpage"'; } ?>><a href="seapg1.php">League News</a></li>
         <li <?php if($thisPage == "Schedule and Scores") { echo 'id="currentpage"'; } ?>><a href="seapg2.php">Schedule and Scores</a></li>
         <li <?php if($thisPage == "Talking Heads") { echo 'id="currentpage"'; } ?>><a href="seapg3.php">Talking Heads</a></li>
-        <li <?php if($thisPage == "LogIn") { echo 'id="currentpage"'; } ?>><a href="seapg4.php">LogIn</a></li>
+        <li <?php if($thisPage == "Log In") { echo 'id="currentpage"'; } ?>><a href="seapg4.php">Log In</a></li>
     </ul>
 </div>
