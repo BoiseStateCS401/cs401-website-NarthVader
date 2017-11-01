1318092e28a393b8d3cc5a0107038c1dc88e3366
NarthVader <nathanberkenmeier@u.boisestate.edu>
2017-11-01 15:48:16 -0600

    tables and queries!

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,6 +1,7 @@
 
 -- Drop table if it already exists so you can start fresh each time.
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS comments;
 
 -- Create a users table in your database. You may have different fields. This isn’t
 -- necessarily the best design.
@@ -11,6 +12,14 @@ CREATE TABLE users (
         name VARCHAR (50) NOT NULL
 );
 
+CREATE TABLE comments (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	message TEXT NOT NULL,
+	posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 -- Insert test data into your table so you have something to start with.
 INSERT INTO users (email, password, name) VALUES('snoopy@peanuts.com', 'w00fWOOF', 'Snoopy');
 INSERT INTO users (email, password, name) VALUES('scooby@whereareyou.com', 'w00fWOOF', 'Scooby');
@@ -19,3 +28,9 @@ INSERT INTO users (email, password, name) VALUES('moana@disney.com', 'born2s@il'
 
 INSERT INTO users (email, password, name) VALUES('nabe0257@colorado.edu', 'password', 'Nate');
 INSERT INTO users (email, password, name) VALUES('sean@school.edu', 'password', 'Sean');
+
+-- use insert to populate the posts table 
+INSERT INTO posts (user_id, message, posted) VALUES(
+	(SELECT id FROM users WHERE username = 'Nate'), 'Go Seahawks!!!', '2015-09-13 12:30:10');
+INSERT INTO posts (user_id, message, posted) VALUES (
+	(SELECT id FROM users WHERE username = 'Sean'), 'Hi, I\'m Sean!', '2015-09-14 8:32:10');
--- public/Dao.php
+++ public/Dao.php
@@ -31,4 +31,102 @@ class Dao
 		$conn = $this->getConnection();
 		return $conn->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
 	}
+
+	public function getUsernameList()
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->query("SELECT username FROM users");
+		return $stmt->fetchAll();
+	}
+	public function userExists($username)
+	{
+		if($this->getUser($username)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public function getUser($username)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT * FROM users WHERE username = :uname");
+		$stmt->bindParam(":uname", $username);
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+	public function filterPostsByKey($key, $value)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("SELECT u.first_name, u.last_name, u.username, p.message, p.posted
+							FROM posts AS p JOIN users AS u ON p.user_id = u.id
+							WHERE $key = :value");
+		$stmt->bindParam(":value", $value);
+		$stmt->execute();
+		return $stmt;
+	}
+	public function getPostsJoinUserName() {
+		$conn = $this->getConnection();
+		return $conn->query("SELECT u.first_name, u.last_name, u.username, p.id, p.message, p.posted
+						 FROM posts AS p JOIN users AS u ON p.user_id = u.id");
+	}
+	public function getPosts()
+	{
+		$conn = $this->getConnection();
+		return $conn->query("SELECT * FROM posts");
+	}
+	public function addPost($username, $message)
+	{
+		$user = $this->getUser($username); 	
+		if($user) {
+			$user_id = $user['id'];
+			$conn = $this->getConnection();
+			$stmt = $conn->prepare("INSERT INTO posts (user_id, message)
+									 VALUES (:user_id, :message)");
+			$stmt->bindParam(":user_id", $user_id);
+			$stmt->bindParam(":message", $message);
+			$stmt->execute();
+		} else {
+			throw new Exception("Invalid user. $username");
+		}
+	}
+	
+	public function deletePostById($id)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("DELETE FROM posts WHERE id = :id");
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		return $stmt->rowCount() == 0 ? false : true;
+	}
+	
+	public function deleteUserPostById($userId, $id)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("DELETE FROM posts WHERE user_id = :user_id AND id = :id");
+		$stmt->bindParam(":user_id", $userId);
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		return $stmt->rowCount() == 0 ? false : true;
+	}
+
+	public function updatePost($id, $message)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("UPDATE posts SET message = :message WHERE id = :id");
+		$stmt->bindParam(":message", $message);
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		return $stmt->rowCount() == 0 ? false : true;
+	}
+	
+	public function updateUserPost($userId, $id, $message)
+	{
+		$conn = $this->getConnection();
+		$stmt = $conn->prepare("UPDATE posts SET message = :message WHERE user_id = :user_id AND id = :id");
+		$stmt->bindParam(":message", $message);
+		$stmt->bindParam(":user_id", $userId);
+		$stmt->bindParam(":id", $id);
+		$stmt->execute();
+		return $stmt->rowCount() == 0 ? false : true;
+	}
 }
