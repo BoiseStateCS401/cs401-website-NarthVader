3f99c4549fbe8bbfeb4e9e8015a5617109609090
NarthVader <nathanberkenmeier@u.boisestate.edu>
2017-11-15 20:18:56 -0700

    should fix alot

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -3,17 +3,7 @@
 
 
 
-// function changeCSS(cssFile, cssLinkIndex) {
-
-//     var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-
-//     var newlink = document.createElement("link");
-//     newlink.setAttribute("rel", "stylesheet");
-//     newlink.setAttribute("type", "text/css");
-//     newlink.setAttribute("href", cssFile);
-
-//     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-// }
+ 
 
 // function registration() {
 //         location.href = "registration.php";
@@ -22,22 +12,21 @@
 $(document).ready( function() { 
 
 /* Define menu click toggle handlers */
-	$("#bwbutton").click(function()
-	{
-		var oldlink = document.getElementsByTagName("link").item(0);
-
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", 'css/bw.css');
-
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-	});
-
-	$("#registration").click(function()
-	{location.href = "registration.php";
-	}
-		);
+	$("#bwbutton").on("click", changeCSS());
+	
+	$("#registration").on("click", function registration() {
+         location.href = "registration.php";});
 	}
 
+	function changeCSS() {
+
+     var oldlink = document.getElementsByTagName("link").item(0);
+
+     var newlink = document.createElement("link");
+     newlink.setAttribute("rel", "stylesheet");
+     newlink.setAttribute("type", "text/css");
+     newlink.setAttribute("href", cssFile);
+
+     document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+ }
  });
