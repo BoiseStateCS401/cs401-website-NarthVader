5efd0053ac577f363ebfebbdaa3ffca642d5036e
NarthVader <nathanberkenmeier@u.boisestate.edu>
2017-11-28 12:54:37 -0700

    comments working!

--- public/index.php
+++ public/index.php
@@ -1,59 +1,54 @@
-<?php
-session_start();
-$thisPage = "The Clink";
-require_once ('header.php');
-require_once ('navigation.php');
-?>
-    <section>
-        <div class="container-fluid">
-            <div class="row-fluid">
-                <h1 id="banner2">Have a GOOD opinion? SHARE HERE!!!</h1>
-                <table class="table table-bordered table-striped">
-                    <caption id="discussionTitle">Register and LogIn To Talk Your Best Trash</caption>
-                    <thead>
-                        <tr>
-                            <th>
-                                User
-                            </th>
-                            <th>
-                                Opinion
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        
-                    </tbody>
+<?php session_start(); $thisPage="The Clink" ; require_once ( 'header.php'); require_once ( 'navigation.php'); require_once ( 'Dao.php');?>
+<section>
+    <div class="container-fluid">
+        <div class="row-fluid">
+            <h1 id="banner2">Have a GOOD opinion? SHARE HERE!!!</h1>
+            <table class="table table-bordered table-striped">
+                <caption id="discussionTitle">Trash Board</caption>
+                <thead>
+                    <tr>
+                        <th>
+                            Trash
+                        </th>
+                        <th>
+                            Date
+                        </th>
+                    </tr>
+                </thead>
+                <?php 
+                $dao = new Dao();
+                $comments= $dao->getComments(); if ($comments) { ?>
+                <table>
+                    <?php foreach ($comments as $comment) { ?>
+                    <tr>
+                        <td>
+                            <?=$comment["message"]; ?>
+                        </td>
+                        <td>
+                            <?=$comment["posted"]; ?>
+                        </td>
+                    </tr>
+                    <?php } ?>
                 </table>
-            </div>
- <?php 
-          if(isset($_SESSION['access']) && ($_SESSION['access']===true)) { ?>
-           <div class="row-fluid">
-                <form method="POST" action="comment-handler.php">
-            <fieldset>
-            
-            <p>
-
-                <label for="comment">comment</label>
-                <input type="text" id="comment" name="comment" maxlength=999 placeholder="Comment here..." value="<?=$_SESSION['presets']['comment'] ?>">
-                <?php
-if (isset($_SESSION['error']['comment']))
-{ ?>
-        <span id="comment" class="error"> <?php echo $_SESSION['error']['comment'] ?></span>
-<?php
-} ?>
-            </p>
-                </form>
-            </div>
+                <?php } ?>
         </div>
-        <?php   }
-        else { ?>
-        <?php  } ?>
+        <?php if(isset($_SESSION[ 'access']) && ($_SESSION[ 'access']===true)) { ?>
+        <div class="row-fluid">
+            <form method="POST" action="comment-handler.php">
+                <fieldset>
 
-    <p>
-    Do you prefer a black and white text? If so...
-    <button id="bwbutton">B/W Text</button>
-    </p>
-    </section>
-    <?php
-require_once ('footer.php');
-?>
+                    <p>
+
+                        <label for="comment">comment</label>
+                        <input type="text" id="comment" name="comment" maxlength=999 placeholder="Comment here..." value="<?=$_SESSION['presets']['comment'] ?>">
+                        <?php if (isset($_SESSION[ 'error'][ 'comment'])) { ?>
+                        <span id="comment" class="error"> <?php echo $_SESSION['error']['comment'] ?></span>
+                        <?php } ?>
+                    </p>
+            </form>
+        </div>
+    </div>
+    <?php } else { ?>Register and Log In to Leave a Comment!
+    <?php } ?>
+</section>
+<?php require_once ( 'footer.php'); ?>
