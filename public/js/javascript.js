995fcca9e41e709a928eb92867af368ce479a4a2
NarthVader <nathanberkenmeier@u.boisestate.edu>
2017-11-30 09:27:32 -0700

    js fix was to switch script from http to https, lol

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
        location.href = "registration.php";
      }); 
     
-    $("#bwbutton").on("click", changeCSS());
-     function changeCSS() {
+   //  $("#bwbutton").on("click", changeCSS());
+   //   function changeCSS() {
 
-       var oldlink = document.getElementsByTagName("link").item(0);
-       var newlink = document.createElement("link");
-       newlink.setAttribute("rel", "stylesheet");
-       newlink.setAttribute("type", "text/css");
-     newlink.setAttribute("href", 'css/bw.css');
-      document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-   };
+   //     var oldlink = document.getElementsByTagName("link").item(0);
+   //     var newlink = document.createElement("link");
+   //     newlink.setAttribute("rel", "stylesheet");
+   //     newlink.setAttribute("type", "text/css");
+   //   newlink.setAttribute("href", 'css/bw.css');
+   //    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+   // };
 
 });
 
