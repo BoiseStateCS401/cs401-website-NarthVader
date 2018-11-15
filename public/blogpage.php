471c0d29cddcb7ed598f5ad8a4ca706681ddc26a
NarthVader <nathanberkenmeier@u.boisestate.edu>
2018-11-15 11:13:36 -0700

    blog

---
+++ public/blogpage.php
@@ -0,0 +1,26 @@
+<?php
+$thisPage = "Blog";
+require_once ('header.php');
+require_once ('navigation.php');
+?>
+    <div>
+        <p id="banner2">Trade News From Around the League</p>
+        <section>
+            <figure>
+                <a href="https://www.cbssports.com/nfl/news/2017-nfl-free-agency-trade-happy-patriots-big-winners-top-receivers-losers/">
+                    <img src="http://www.seahawks.com/sites/seahawks.com/files/styles/borealis_article_image_respondlarge/public/_article_thumbnails/rm1_54681.jpg?itok=h4i-qBir&timestamp=1504576653" alt="Richardson Trade" title="NFL Trade News" />
+                </a>
+                <figcaption id="caption"><a href="https://www.cbssports.com/nfl/news/2017-nfl-free-agency-trade-happy-patriots-big-winners-top-receivers-losers/">NFL Trade News</a></figcaption>
+            </figure>
+            <p id="banner2">League Injury Report</p>
+            <figure>
+                <a href="http://www.espn.com/nfl/injuries">
+                    <img src="http://dailysnark.com/wp-content/uploads/2015/02/Screen-Shot-2015-02-01-at-7.55.58-PM.png" alt="Injury Report" title="Injury Report" />
+                </a>
+                <figcaption id="caption"><a href="http://www.espn.com/nfl/injuries">Weekly Injury Report</a></figcaption>
+            </figure>
+        </section>
+    </div>
+<?php
+require_once ('footer.php');
+?>
