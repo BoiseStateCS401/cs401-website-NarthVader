d3e58db7dba0a0cb07eac95191d9445f22771149
NarthVader <nathanberkenmeier@u.boisestate.edu>
2018-11-15 11:26:44 -0700

    blog

--- public/blogpage.php
+++ public/blogpage.php
@@ -8,7 +8,9 @@ require_once ('navigation.php');
         <section>
             <p> <b>What Is Immutability???</b>
                 Immutability is a concept that once created, data cannot be changed. This stands in stark contrast to the concept of Object Oriented programming
-                in which everything relies on the behavior of side-effects in order to run your program. 
+                in which everything relies on the behavior of side-effects in order to run your program. As anyone who has programmed in an OO language before, namely
+                Java because this is a Java focused department, if you aren't careful with your code and treatment of side-effects, bugs are nearly impossible to track down, and unless your code is highly modularized, any changes to your program down the road has a high probability of causing havoc. Great, you've changed the onClick handler to give you some cool effect, but now your whole program is translated to Martian. This is a hyperbolic example, but I've had simple changes to small parts of code completely wreck larger aspects due to unintended side-effects. <br>
+                next P
             </p>
             
         </section>
